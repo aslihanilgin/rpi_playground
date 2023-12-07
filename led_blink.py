a69c0f6f7987f92cc81bdfbb5dd1892586faefe3
Aslihan Ilgin <aslihanilginn@gmail.com>
2023-12-07 19:59:04 +0000

    simple blink app

---
+++ led_blink.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+
+while True:
+    print("in the loop")
+    GPIO.output(18, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(18, GPIO.LOW)
+    sleep(1)
